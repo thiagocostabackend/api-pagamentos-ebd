f2e089d207fcdf41c2effad5ea1588cae117d968
thiagocostabackend <thiagocosta.backend@gmail.com>
2026-02-07 00:04:54 -0300

    feat: configura conexão com MySQL usando variáveis de ambiente

--- config/database.py
+++ config/database.py
@@ -1,3 +1,16 @@
-"""
-Responsável pela configuração e conexão com o banco de dados.
-"""
+#Responsável pela configuração e conexão com o banco de dados.
+
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
+def get_connection(): #Cria e retorna uma conexão com o banco de dados MySQL utilizando variáveis de ambiente
+    return mysql.connector.connect(
+        host = os.getenv("DB_HOST"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
+        database = os.getenv("DB_NAME")
+    )
